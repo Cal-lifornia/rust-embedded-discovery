507469793b302c4ad92d358b94e5df953abf675f
Cal-lifornia <76858909+Cal-lifornia@users.noreply.github.com>
2025-01-10 14:10:32 +1100

    Added the testing pages and updated flake.nix to work on my mac.

--- flake.nix
+++ flake.nix
@@ -23,54 +23,42 @@
         };
       in
       {
-        # overlays.default = final: prev: {
-        #   rustToolchain =
-        #     let
-        #       rust = prev.rust-bin;
-        #     in
-        #     rust.selectLatestNightlyWith (
-        #       toolchain:
-        #       toolchain.default.override {
-        #         extensions = [
-        #           "rust-src"
-        #           "rustfmt"
-        #           "clippy"
-        #           "llvm-tools"
-        #         ];
-        #       }
-        #     );
-
-        # };
         devShells.default =
           with pkgs;
           mkShell {
-            buildInputs = [
-              openssl
-              pkg-config
-              rust-analyzer
-              cargo-deny
-              cargo-edit
-              cargo-watch
-              cargo-insta
-              probe-rs-tools
-              minicom
-              gdb
-              udev
-              cargo-binutils
-              usbutils
-              (rust-bin.selectLatestNightlyWith (
-                toolchain:
-                toolchain.default.override {
-                  extensions = [
-                    "rust-src"
-                    "rustfmt"
-                    "clippy"
-                    "llvm-tools"
-                  ];
-                  targets = [ "thumbv7em-none-eabihf" ];
-                }
-              ))
-            ];
+            buildInputs =
+              [
+                openssl
+                pkg-config
+                rust-analyzer
+                cargo-deny
+                cargo-edit
+                cargo-watch
+                cargo-insta
+                probe-rs-tools
+                minicom
+                cargo-binutils
+                (rust-bin.selectLatestNightlyWith (
+                  toolchain:
+                  toolchain.default.override {
+                    extensions = [
+                      "rust-src"
+                      "rustfmt"
+                      "clippy"
+                      "llvm-tools"
+                    ];
+                    targets = [ "thumbv7em-none-eabihf" ];
+                  }
+                ))
+              ]
+              ++ (lib.optionals pkg.stdenv.isLinux [
+                gdb
+                usbutils
+              ])
+              ++ (lib.optionals pkg.stdenv.isDarwin [
+                gcc-arm-embedded
+                darwin.lsusb
+              ]);
 
             env = {
               # RUST_SRC_PATH = "${pkgs.rustToolchain}/lib/rustlib/src/rust/library";
